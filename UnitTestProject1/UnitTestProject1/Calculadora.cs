200c597a6a6ac157b5f0ec916b4bad80b4f1ae76
jorgitopeke <jorgito.c.2011@gmail.com>
2014-11-10 09:49:12 -0600

    prue

---
+++ UnitTestProject1/UnitTestProject1/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject1
+{
+    class Calculadora
+    {
+        public int suma(int nu1, int nu2) {
+            
+            return nu1+nu2;
+        }
+        public int resta(int nu1, int nu2)
+        {
+
+            return nu1 - nu2;
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculadora cal = new Calculadora();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            Assert.AreEqual(4, cal.suma(2, 2));
+        }
+         [TestMethod]
+        public void TestMethod2()
+        {
+            
+            Assert.AreEqual(0, cal.resta(2, 2));
+        }
+    }
+}
